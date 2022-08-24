7e8df822416e018c9234f13297506c53999ebdaf
ocousin <ocousin@xilinx.com>
2022-08-24 16:47:51 +0100

    add first code

---
+++ Model/CNN-1D/build_hardware.py
@@ -0,0 +1,27 @@
+#BUILD IP WITH FINN
+import finn.builder.build_dataflow as build
+import finn.builder.build_dataflow_config as build_cfg
+import os
+import shutil
+
+model_file = "finn_QWNet111.onnx"
+
+estimates_output_dir = "output_estimates_only"
+
+#Delete previous run results if exist
+if os.path.exists(estimates_output_dir):
+    shutil.rmtree(estimates_output_dir)
+    print("Previous run results deleted!")
+
+
+cfg_estimates = build.DataflowBuildConfig(
+    output_dir          = estimates_output_dir,
+    mvau_wwidth_max     = 80,
+    target_fps          = 1000000,
+    synth_clk_period_ns = 10.0,
+    fpga_part           = "xc7z020clg400-1",
+    steps               = build_cfg.estimate_only_dataflow_steps,
+    generate_outputs=[
+        build_cfg.DataflowOutputType.ESTIMATE_REPORTS,
+    ]
+)
